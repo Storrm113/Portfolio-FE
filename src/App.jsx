ef5254e48f154c2b7d642c7921de5d538b7a7b37
Storrm113 <darinburden@Gmail.com>
2025-05-05 16:55:31 -0400

    undid some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import './index.css';
 
 function App() {
   return (
@@ -15,3 +21,5 @@ function App() {
     </Router>
   );
 }
+
+export default App;
